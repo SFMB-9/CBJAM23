e2225910d3a8d3c6f8a81ad631c7e5b64273b995
m3llad0 <a01655451@tec.mx>
2023-01-20 14:56:34 -0600

    using router

--- gameJam-API/index.js
+++ gameJam-API/index.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 db
-// app.use(router)
+app.use(router)
 
 app.listen(process.env.PORT, () =>{
     console.log(`Connected on port ${process.env.PORT}`)
