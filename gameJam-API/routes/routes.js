f82310e1b9a72583424b72faf3baf3fa36a7e5ac
m3llad0 <a01655451@tec.mx>
2023-01-20 16:45:29 -0600

    highscores route

--- gameJam-API/routes/routes.js
+++ gameJam-API/routes/routes.js
@@ -4,6 +4,7 @@ const scoreController = require('../controller/scoreController');
 const router = require('express').Router();
 
 router.post('/newScore', scoreController.setScore);
+router.get('/highScores', scoreController.highScores);
 
 
 module.exports = router;
