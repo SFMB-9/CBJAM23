02f4488a2462335c616fb6f0e652d6c2d22e6cba
m3llad0 <a01655451@tec.mx>
2023-01-20 14:32:23 -0600

    express app set up

---
+++ gameJam-API/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const router = require('./routes/routes');
+const db = require('./database/db');
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+db
+// app.use(router)
+
+app.listen(process.env.PORT, () =>{
+    console.log(`Connected on port ${process.env.PORT}`)
+})
