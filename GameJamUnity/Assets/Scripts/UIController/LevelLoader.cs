061ed767a9f90b518c1c646a47244bb4831f099b
Luisjakg <52958375+Luisjakg@users.noreply.github.com>
2023-01-23 16:02:10 -0600

    Added Scene Transitions

---
+++ GameJamUnity/Assets/Scripts/UIController/LevelLoader.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] private Animator transition;
+    [SerializeField] private float transitionTime = 1f;
+    public static LevelLoader instance;
+    
+    private bool isTransitioning = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        instance = this;
+    }
+
+    public void LoadNextLevel()
+    {
+        if (isTransitioning) return;
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+    
+    
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        if (isTransitioning) yield break;
+            isTransitioning = true;
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        isTransitioning = false;
+        SceneManager.LoadScene(levelIndex);
+    }
+
+
+}
