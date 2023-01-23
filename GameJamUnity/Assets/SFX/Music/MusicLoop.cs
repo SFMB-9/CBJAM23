0a0bb237366eda8e03de8f646e9705e779cc4be0
Luisjakg <52958375+Luisjakg@users.noreply.github.com>
2023-01-22 21:28:03 -0600

    Music

---
+++ GameJamUnity/Assets/SFX/Music/MusicLoop.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MusicLoop : MonoBehaviour
+{ 
+    [SerializeField] private AudioSource introSource;
+    [SerializeField] private AudioSource loopSource;
+    [SerializeField] private AudioClip intro;
+    [SerializeField] private AudioClip loop;
+    
+
+    private void Start()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        if (introSource.isPlaying || loopSource.isPlaying) return;
+        StartCoroutine(PlayLooping());
+    }
+
+    private IEnumerator PlayLooping()
+    {
+        introSource.Play();
+        yield return new WaitForSeconds(introSource.clip.length - 1.1f);
+        loopSource.Play();
+        loopSource.PlayScheduled(AudioSettings.dspTime + loop.length);
+    }
+
+}
--- GameJamUnity/Assets/Scripts/NPC/Mover.cs
+++ GameJamUnity/Assets/Scripts/NPC/Mover.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.Timeline.Actions;
 using UnityEngine;
 using UnityEngine.AI;
 
