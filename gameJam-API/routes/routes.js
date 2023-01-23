653fb2a64b1f79b1b2bbbc0eb29e8ce104661638
m3llad0 <a01655451@tec.mx>
2023-01-22 23:07:17 -0600

    added multer middleware for form data support

--- gameJam-API/routes/routes.js
+++ gameJam-API/routes/routes.js
@@ -1,9 +1,10 @@
-const { model } = require('mongoose');
+const multer  = require('multer')
+const upload = multer()
 const scoreController = require('../controller/scoreController');
 
 const router = require('express').Router();
 
-router.post('/newScore', scoreController.setScore);
+router.post('/newScore', upload.none() ,scoreController.setScore);
 router.get('/highScores', scoreController.highScores);
 
 
