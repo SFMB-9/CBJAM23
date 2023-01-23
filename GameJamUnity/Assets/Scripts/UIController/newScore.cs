1ea99bac69cd3d994d494ed90b3411bd4325600d
m3llad0 <dmelladooliveros@gmail.com>
2023-01-22 23:20:03 -0600

    script for sending name and score to the database

---
+++ GameJamUnity/Assets/Scripts/UIController/newScore.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;  
+using TMPro;
+
+public class newScore : MonoBehaviour
+{
+    string initials;
+    int score = 0;
+
+    IEnumerator NewScore(int score, string name)
+    {
+        WWWForm form = new WWWForm();
+
+        form.AddField("name", name);
+        form.AddField("score", score);
+        
+        UnityWebRequest www = UnityWebRequest.Post("http://localhost:8000/newScore", form);
+        yield return www.SendWebRequest();
+
+        if(www.result != UnityWebRequest.Result.Success){
+            Debug.Log("Error");
+        }else{
+            Debug.Log("Score saved!");
+        }
+    }
+
+    public void readString(string text)
+    {
+        initials = text;
+        Debug.Log(initials);
+    }
+
+    public void onClick()
+    {
+        StartCoroutine(NewScore(score, initials));
+    }
+}
